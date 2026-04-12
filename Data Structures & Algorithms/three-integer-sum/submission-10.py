55100fea493f2a68fb3877e57607cefad22bdbdb
Hoakin24 <89337901+Hoakin24@users.noreply.github.com>
2026-04-12 20:06:52 +0800

    Add: three-integer-sum - submission-10

---
+++ Data Structures & Algorithms/three-integer-sum/submission-10.py
@@ -0,0 +1,70 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # sorted_nums = sorted(nums)
+        # nums_len = len(nums)
+        # outputs = []
+
+        # for i in range(nums_len):
+        #     if sorted_nums[i] > 0:
+        #         break
+
+        #     if i > 0 and sorted_nums[i] == sorted_nums[i - 1]:
+        #         continue
+
+        #     left = i + 1
+        #     right = nums_len - 1
+            
+        #     while left < right:
+        #         current_total = sorted_nums[left] + sorted_nums[right] + sorted_nums[i]
+        #         if current_total < 0:
+        #             left += 1
+        #         elif current_total > 0:
+        #             right -= 1
+        #         else:
+        #             outputs.append([sorted_nums[i], sorted_nums[left], sorted_nums[right]])
+        #             left += 1
+        #             right -= 1
+                
+        #             while left < right and sorted_nums[left] == sorted_nums[left - 1]:
+        #                 left += 1
+
+        #             while left < right and sorted_nums[right] == sorted_nums[right + 1]:
+        #                 right -= 1 
+
+        # return outputs
+
+        sorted_nums = sorted(nums)
+        nums_len = len(nums)
+        output = []
+
+        for i in range(nums_len):
+            current_num = sorted_nums[i]
+
+            if current_num > 0:
+                break
+            
+            if i > 0 and current_num == sorted_nums[i-1]:
+                continue
+
+            left = i + 1
+            right = nums_len - 1
+
+            while left < right:
+                current_total = current_num + sorted_nums[left] + sorted_nums[right]
+
+                if current_total < 0:
+                    left += 1
+                elif current_total > 0:
+                    right -= 1
+                else:
+                    output.append([current_num, sorted_nums[left], sorted_nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and sorted_nums[left] == sorted_nums[left-1]:
+                        left += 1
+
+                    while left < right and sorted_nums[right] == sorted_nums[right+1]:
+                        right -= 1
+
+        return output
